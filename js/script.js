c16a56a22112e919a6b976fcff9f8e3d31a81f5e
Ahmad Raza <raza2157@gmail.com>
2018-03-23 16:32:29 -0400

    Added Animations, Fixed Bug
    - Added an animation for both the login page and signup page
    - Fixed bug where string wasn't checked if it was empty

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@
 function emailValidation() {
     var str = document.getElementById("password_reset_email").value;
     for (i = 0; i < str.length; i++) {
-        if (str[i] == '@') {
+        if (str.length != 0 && str.includes('@')) {
             document.getElementById("password_reset_alert").classList.remove('alert-warning');
             document.getElementById("password_reset_alert").classList.add('alert-success');
             document.getElementById("password_reset_alert").innerText = "Success! Please check your email for further instructions.";
             document.getElementById("password_reset_alert").style.display = "block";
         }
-        else {
+        else
+            {
             document.getElementById("password_reset_alert").classList.remove('alert-success');
             document.getElementById("password_reset_alert").classList.add('alert-warning');
             document.getElementById("password_reset_alert").innerText = "Please enter a valid email address and try again.";
