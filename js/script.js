bd2fe1632f1e534909fdc9c3a7f2b436254782fd
Ahmad Raza <raza2157@gmail.com>
2018-03-23 12:50:16 -0400

    Fixed JS Error
    - Renamed ID tag for the JS code to run properly

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@ function emailValidation() {
     var str = document.getElementById("password_reset_email").value;
     for (i = 0; i < str.length; i++) {
         if (str[i] == '@') {
-            document.getElementById("password_reset_success_alert").classList.remove('alert-warning');
-            document.getElementById("password_reset_success_alert").classList.add('alert-success');
-            document.getElementById("password_reset_success_alert").innerText = "Success! Please check your email for further instructions.";
-            document.getElementById("password_reset_success_alert").style.display = "block";
+            document.getElementById("password_reset_alert").classList.remove('alert-warning');
+            document.getElementById("password_reset_alert").classList.add('alert-success');
+            document.getElementById("password_reset_alert").innerText = "Success! Please check your email for further instructions.";
+            document.getElementById("password_reset_alert").style.display = "block";
         }
         else {
-            document.getElementById("password_reset_success_alert").classList.remove('alert-success');
-            document.getElementById("password_reset_success_alert").classList.add('alert-warning');
-            document.getElementById("password_reset_success_alert").innerText = "Please enter a valid email address and try again.";
-            document.getElementById("password_reset_success_alert").style.display = "block";
+            document.getElementById("password_reset_alert").classList.remove('alert-success');
+            document.getElementById("password_reset_alert").classList.add('alert-warning');
+            document.getElementById("password_reset_alert").innerText = "Please enter a valid email address and try again.";
+            document.getElementById("password_reset_alert").style.display = "block";
         }
     }
 }
